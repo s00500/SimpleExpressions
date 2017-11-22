d18ae3207fb6901f9b92dfbf4fd463dcc55e96c0
Lukas Bachschwell <lukas@lbsfilm.at>
2017-11-22 16:22:42 +0100

    EVEN NOT DYNAMIC

--- examples/mouth.ino
+++ examples/mouth.ino
@@ -4,26 +4,23 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   SimpleExpressions.init(13, 14);
+  //SimpleExpressions._tone(440, 5000,1);
 
 }
 
 void loop() {
 
-  SimpleExpressions.printMouthShape(0);
-  //SimpleExpressions.writeMouth("zeros");
+  SimpleExpressions.writeMouth("zeros");
   delay(500);
   SimpleExpressions.printMouthShape(1);
-  //SimpleExpressions.writeMouth("happySmall");
   delay(500);
-  //SimpleExpressions.writeMouth("happyFull");
-  SimpleExpressions.printMouthShape(2);
+  SimpleExpressions.writeMouth("happyFull");
   delay(500);
-  SimpleExpressions.printMouthShape(3);
-  //SimpleExpressions.writeMouth("zeros");
+  SimpleExpressions.writeMouth("zeros");
   delay(500);
-  //SimpleExpressions.writeMouth("sadSmall");
-  SimpleExpressions.printMouthShape(4);
+  SimpleExpressions.writeMouth("sadFull");
   delay(500);
-
+  SimpleExpressions.writeMouth("sadSmall");
+  delay(500);
 
 }
